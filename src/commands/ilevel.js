3a6ced3b025639247007a131bee4872a8dff2f7d
OMGDuke <lex1989@gmail.com>
2016-10-30 01:34:45 +0100

    ilevel command fully working

--- src/commands/ilevel.js
+++ src/commands/ilevel.js
@@ -5,24 +5,44 @@ class Ilevel {
   constructor() {
     this.url = "https://eu.api.battle.net/wow/character/";
     this.urlOptions = "?fields=items&locale=en_GB&apikey="
+    this.json = {};
   }
 
   send(commands) {
-    if (commands.length < 3) {return "You must provide a server and character name"};
+    if (commands.length < 3) {return "You must provide a server and character" +
+    " name"};
     this.server = commands[1];
     this.character = commands[2];
+    this.apiRequest(this.buildUrl());
+    return this.sendMessage();
   }
 
   apiRequest(url) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, false ); // false for synchronous request
     xmlHttp.send( null );
-    return JSON.parse(xmlHttp.responseText);
+    this.json = JSON.parse(xmlHttp.responseText);
   }
 
   buildUrl() {
     return this.url + this.server + "/" + this.character + this.urlOptions +
     process.env.WOW_API_KEY;
+  }
+
+  parseJson() {
+    return JSON.stringify(this.json.items.averageItemLevelEquipped);
+  }
+
+  buildResponse(ilevel) {
+    return this.json.name + "'s iLevel: " + ilevel;
+  }
+
+  sendMessage() {
+    if("items" in this.json) {
+      return this.buildResponse(this.parseJson());
+    } else {
+      return this.json.reason;
+    }
   }
 }
 
--- test/ilevelSpec.js
+++ test/ilevelSpec.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
-
 let expect = require("chai").expect;
+let sinon = require("sinon")
+
 import Ilevel from '../src/commands/ilevel';
 
 let ilevel = new Ilevel();
-let commandArray = ["!ilevel", "Server Name", "Character Name"];
+let commandArray = ["!ilevel", "draenor", "omgduke"];
 let url = "https://eu.api.battle.net/wow/character/";
 let urlOptions = "?fields=items&locale=en_GB&apikey=";
-
+let testUrl = url + commandArray[1] + "/" + commandArray[2] + urlOptions +
+process.env.WOW_API_KEY;
+let response = "Omgduke's iLevel: 870";
 
 describe("Ilevel", function() {
   describe("Initialize", function() {
@@ -23,12 +26,12 @@ describe("Ilevel", function() {
   describe("send", function() {
     it("take in an array and stores the server name", function() {
       ilevel.send(commandArray)
-      expect(ilevel.server).to.equal("Server Name");
+      expect(ilevel.server).to.equal("draenor");
     });
 
     it("take in an array and stores the character name", function() {
       ilevel.send(commandArray)
-      expect(ilevel.character).to.equal("Character Name");
+      expect(ilevel.character).to.equal("omgduke");
     });
 
     it("returns an error message if arguements not provided", function() {
@@ -39,10 +42,33 @@ describe("Ilevel", function() {
 
   describe("buildUrl", function() {
     it("builds a url from the command received", function() {
-      let testUrl = url + commandArray[1] + "/" + commandArray[2] + urlOptions +
-      process.env.WOW_API_KEY;
       ilevel.send(commandArray);
       expect(ilevel.buildUrl()).to.equal(testUrl);
-    })
+    });
   });
+
+  describe("parseJson", function() {
+    it("returns the ilevel", function() {
+      ilevel.json = {items: {averageItemLevelEquipped: 870}}
+      expect(ilevel.parseJson()).to.equal('870');
+    });
+  });
+
+  describe("buildResponse", function() {
+    it("builds a string to respond with", function() {
+      ilevel.send(commandArray);
+      expect(ilevel.buildResponse(870)).to.equal(response);
+    })
+  })
+
+  describe("sendMessage", function() {
+    it("Sends a correct response if data is valid", function() {
+      ilevel.send(commandArray);
+      expect(ilevel.sendMessage()).to.equal(response);
+    });
+    it("provides an error message if data is invalid", function() {
+      ilevel.send([1, "server", "character"]);
+      expect(ilevel.sendMessage()).to.equal("Realm not found.");
+    });
+  })
 });
