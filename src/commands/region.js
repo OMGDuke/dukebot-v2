0ec12022c913e5eba2e6815ed470dab2eb50d4f4
Alex Robertson <lex1989@gmail.com>
2017-02-08 13:59:33 +0000

    Updated region command

--- src/commands/region.js
+++ src/commands/region.js
@@ -9,7 +9,7 @@ class Region {
 
   send(commands) {
     if (commands.length === 1) {return this.currentRegion};
-    if (commands[1] != "us" && commands[1] != "eu") {return "Only us and eu are supported"}
+    if (commands[1] != "us" && commands[1] != "eu") {return "Only us and eu are supported"};
     this.currentRegion = commands[1].toLowerCase();
     return "Region changed to " + this.currentRegion.toUpperCase();
   }
